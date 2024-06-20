d0c459f332de6d17516ff720cba70e51a86b97c8
Dev Iskalo <deviskalo2000@gmail.com>
2024-06-20 16:29:22 +0000

    initial commits

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,10 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+  apiKey: "AIzaSyCl9RFlGnFJGQbR49AZ-NfH46mif924K6M",
+  authDomain: "blogging-web-7d8d6.firebaseapp.com",
+  projectId: "blogging-web-7d8d6",
+  storageBucket: "blogging-web-7d8d6.appspot.com",
+  messagingSenderId: "1080777279409",
+  appId: "1:1080777279409:web:4c7724fba692b7f81a1f90",
 };
 
 firebase.initializeApp(firebaseConfig);
