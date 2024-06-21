6f3ad7480dd8945f2ec62358b5f32a0be45108a8
Dev Iskalo <deviskalo2000@gmail.com>
2024-06-21 18:15:12 +0000

    congured the dashboard for authentication

---
+++ public/js/dashboard.js
@@ -0,0 +1,25 @@
+let ui = new firebaseui.auth.AuthUI(auth);
+let login = document.querySelector(".login");
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    login.style.display = "none";
+  } else {
+    setupLoginButton();
+  }
+});
+
+const setupLoginButton = () => {
+  ui.start("#loginUI", {
+    callbacks: {
+      signInSuccessWithAuthResule: function (authResult, redirectURL) {
+        login.style.display = "none";
+        return false;
+      },
+    },
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  });
+};
+
+setupLoginButton();
